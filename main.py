ba7a89c8653aae6d7d73b680675a2e8d7de79c0c
Olena Shapoval <OlenaShapoval93@gmail.com>
2025-03-07 17:55:24 +0100

    module 4, hw 4

--- main.py
+++ main.py
@@ -1,16 +1,59 @@
-# This is a sample Python script.
+def parse_input(user_input):
+    cmd, *args = user_input.split()
+    cmd = cmd.strip().lower()
+    return cmd, *args
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def add_contact(args, contacts):
+    try:
+        name, phone = args
+        contacts[name] = phone
+        return "Contact added."
+    except ValueError:
+        return "Only 2 values expected"
+
+def change_contact(args, contacts):
+    try:
+        name, phone = args
+        if name in contacts:
+            contacts[name] = phone
+            return "Contact updated"
+        else:
+            return "Contact not found"
+    except ValueError:
+        return "Only 2 values expected"
+
+def show_phone(name, contacts):
+    if name in contacts:
+        return contacts[name]
+    else:
+        return "User was not found"
+
+def  show_all(contacts):
+    return contacts
+
+def main():
+    contacts = {}
+    print("Welcome to the assistant bot!")
+    while True:
+        user_input = input("Enter a command: ")
+        command, *args = parse_input(user_input)
+        if command in ["close", "exit"]:
+            print("Good bye!")
+            break
+        elif command == "hello":
+            print("How can I help you?")
+        elif command == "add":
+            print(add_contact(args, contacts))
+        elif command == "change":
+            print(change_contact(args, contacts))
+        elif command == "phone":
+            print(show_phone(args[0], contacts))
+        elif command == "all":
+            print(show_all(contacts))
+        else:
+            print("Invalid command.")
+
+if __name__ == "__main__":
+    main()
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
